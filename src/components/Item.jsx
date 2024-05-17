75f78c3cfaadf879fbde66fda4efb3c5742085e9
RingKim1 <kgh9393@naver.com>
2024-05-17 21:11:23 +0900

    feat : Item component add

---
+++ src/components/Item.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import Swal from "sweetalert2";
+
+const Item = ({ el, setToDos }) => {
+  const removeToDo = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        setToDos((todos) => todos.filter((el) => el.id !== id));
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your list has been deleted.",
+          icon: "success",
+        });
+      }
+    });
+  };
+
+  const toggleToDo = (id) => {
+    setToDos((todos) =>
+      todos.map((el) =>
+        el.id === id ? { ...el, completed: !el.completed } : el
+      )
+    );
+  };
+  return (
+    <div>
+      <li
+        key={el.id}
+        style={{
+          margin: "20px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h3 style={{ margin: "5px" }}>{el.title}</h3>
+        <div style={{ margin: "5px" }}>{el.content}</div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <button
+            style={{ margin: "5px" }}
+            onClick={() => {
+              toggleToDo(el.id);
+            }}
+          >
+            {el.completed ? `취소` : `완료`}
+          </button>
+          <button
+            style={{ margin: "5px" }}
+            onClick={() => {
+              removeToDo(el.id);
+            }}
+          >
+            삭제
+          </button>
+        </div>
+      </li>
+    </div>
+  );
+};
+
+export default Item;

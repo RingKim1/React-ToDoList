980819a0af789093c76f538e760cf77a22c19e27
RingKim1 <kgh9393@naver.com>
2024-05-17 20:18:28 +0900

    fix : file route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import Form from "./component/Form";
-import List from "./component/List";
+import Form from "./components/Form";
+import List from "./components/List";
 
 function App() {
   const initialState = [
